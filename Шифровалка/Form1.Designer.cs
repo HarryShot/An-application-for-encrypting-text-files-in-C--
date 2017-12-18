1133fd48e99df27ce3e39098ad183b8f7b52db11
HarryShot <h.krestiyaninov@gmail.com>
2017-12-18 14:59:28 +0200

    load

---
+++ Шифровалка/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Шифровалка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(55, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Зашифровать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(220, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Расшифровать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(385, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите имя файла :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 214);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Шифровалка/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Шифровалка
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str2 = "";
+            string l = textBox1.Text.ToString()+".txt";
+            if (l == ".txt")
+            {
+                MessageBox.Show("Пожалуйста введите имя файла");
+            }
+            else
+            {
+                if (File.Exists(l))
+                {
+                    StreamReader streamReader = new StreamReader(l); //Открываем файл для чтения
+                    string str = ""; //Объявляем переменную, в которую будем записывать текст из файла
+
+                    while (!streamReader.EndOfStream) //Цикл длиться пока не будет достигнут конец файла
+                    {
+                        str += streamReader.ReadLine(); //В переменную str по строчно записываем содержимое файла
+                    }
+                    char[] arr = str.ToCharArray();
+                    for (int i = 0; i < str.Length; i++)
+                    { 
+                       int a = i/10;
+                      // if (a % 2 == 0)
+                      // {
+                      //     str2 += ((char)(arr[i] + 17)).ToString();
+                      // }
+                      // else 
+                      // {
+                           str2 += ((char)(arr[i] - 6)).ToString();
+                      // }
+                         }
+                   
+                    StreamWriter write_text;  //Класс для записи в файл
+                    FileInfo file = new FileInfo("code.txt");
+                    if (file.Exists == true) //Если файл существует
+                    {
+                        file.Delete(); //Удаляем
+                    }
+                    write_text = file.AppendText(); //Дописываем инфу в файл, если файла не существует он создастся
+                    write_text.WriteLine(str2); //Записываем в файл текст из текстового поля
+                    write_text.Close(); // Закрываем файл
+                    MessageBox.Show("Файл зашифрован");
+                   
+                }
+                else {
+                    MessageBox.Show("Введите коррекное имя файла"); }
+               
+            }
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str2 = "";
+            string l = textBox1.Text.ToString() + ".txt";
+            if (l == ".txt")
+            {
+                MessageBox.Show("Пожалуйста введите имя файла");
+            }
+            else
+            {
+                if (File.Exists(l))
+                {
+                    StreamReader streamReader = new StreamReader(l); //Открываем файл для чтения
+                    string str = ""; //Объявляем переменную, в которую будем записывать текст из файла
+
+                    while (!streamReader.EndOfStream) //Цикл длиться пока не будет достигнут конец файла
+                    {
+                        str += streamReader.ReadLine(); //В переменную str по строчно записываем содержимое файла
+                    }
+                    char[] arr = str.ToCharArray();
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        int a = i / 10;
+                      //  if (a % 2 == 0)
+                       // {
+                       //     str2 += ((char)(arr[i] - 17)).ToString();
+                      //  }
+                       // else
+                       // {
+                            str2 += ((char)(arr[i] + 6)).ToString();
+                       // }
+                    }
+                    StreamWriter write_text;  //Класс для записи в файл
+                    FileInfo file = new FileInfo("uncode.txt");
+                    if (file.Exists == true) //Если файл существует
+                    {
+                        file.Delete(); //Удаляем
+                    }
+                    write_text = file.AppendText(); //Дописываем инфу в файл, если файла не существует он создастся
+                    write_text.WriteLine(str2); //Записываем в файл текст из текстового поля
+                    write_text.Close(); // Закрываем файл
+                    MessageBox.Show("Файл расшифрован");
+
+                }
+                else
+                {
+                    MessageBox.Show("Введите коррекное имя файла");
+                }
+
+            }
+        }
+    }
+}
